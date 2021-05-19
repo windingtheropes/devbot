373b7fa9c4e6c58192ff43495e6795cf6c366fd1
WindingTheRopes <17016045+windingtheropes@users.noreply.github.com>
2021-05-19 13:54:35 -0400

    Update downtime.js

--- src/commands/downtime.js
+++ src/commands/downtime.js
@@ -1,7 +1,16 @@
 const Discord = require('discord.js')
+const Client = new Discord.Client()
+const {operators} = require('../config.json')
 module.exports = downtime
 
 function downtime(args, command)
 {
-
+    if(operators.indexOf(command.author.id) > -1)
+	{
+        command.channel.send("you are permitted")
+	}
+    else
+	{
+		command.channel.send("Insufficient permissions.")
+	}
 }
