dee125cb2e37057bfe5025236eccb4f7fb8ad9e7
windingtheropes <17016045+windingtheropes@users.noreply.github.com>
2021-07-26 10:33:51 -0400

    add archive folder and move word filters there

--- features/message/wordFilter.js
+++ features/message/wordFilter.js
@@ -1,40 +1,40 @@
-const fs = require('fs')
-const path = require('path')
-const bannedWords = []
+// const fs = require('fs')
+// const path = require('path')
+// const bannedWords = []
 
-loadWords()
-function loadWords() {
-    try
-    {
-        const wordsPath = path.join(__dirname, '../../config/words')
-        fs.readdir(wordsPath, (err, files) => {
-          files.forEach(file => {
-            const data = fs.readFileSync(path.join(wordsPath, file), 'utf8')
-            const words = data.split(',')
-            words.forEach(word => {
-                bannedWords.push(word)
-            })
+// loadWords()
+// function loadWords() {
+//     try
+//     {
+//         const wordsPath = path.join(__dirname, '../../config/words')
+//         fs.readdir(wordsPath, (err, files) => {
+//           files.forEach(file => {
+//             const data = fs.readFileSync(path.join(wordsPath, file), 'utf8')
+//             const words = data.split(',')
+//             words.forEach(word => {
+//                 bannedWords.push(word)
+//             })
 
-          })
-    })
-}
-catch
-{
-    //no word lists/another error
-}
-}
+//           })
+//     })
+// }
+// catch
+// {
+//     //no word lists/another error
+// }
+// }
 
-module.exports = {
-    enabled: false,
-    callback: (message) => {
-        const { content } = message
-    bannedWords.forEach(word => {
-        if(content.includes(` ${word} `) || content.startsWith(` ${word}`) || content.endsWith(`${word} `) || content === word)
-        {
-            message.delete()
-        }
-    })
-    }
-}
+// module.exports = {
+//     enabled: false,
+//     callback: (message) => {
+//         const { content } = message
+//     bannedWords.forEach(word => {
+//         if(content.includes(` ${word} `) || content.startsWith(` ${word}`) || content.endsWith(`${word} `) || content === word)
+//         {
+//             message.delete()
+//         }
+//     })
+//     }
+// }
 
 
