87fa77c105dce2eb07fdeecea45f4ec6fef228cc
windingtheropes <17016045+windingtheropes@users.noreply.github.com>
2021-12-20 20:10:01 -0500

    update channel info

---
+++ commands/serverinfo.js
@@ -0,0 +1,24 @@
+
+
+const Discord = require('discord.js')
+const getTimePassed = require('../utils/getTimeFromTimestamp')
+module.exports = {
+    name: "serverinfo",
+    miniDescription: 'Get information about the current server.',
+    description: 'Get information about the current server.',
+    commands: ['serverinfo'],
+    callback: async (message, args, text, client) => {
+    
+           const embed = new Discord.MessageEmbed()
+           .setColor('#0099ff')
+           .setTitle(`${message.guild.name} information`)
+           .addField('ID', message.guild.id, false)
+           .addField('Creation Date', `${getTimePassed(Date.now() - message.guild.createdAt)} ago (${new Date(message.guild.createdAt).toUTCString()})`)
+            return message.reply({embeds: [embed], allowedMentions: {repliedUser: false}})
+          
+
+
+    }
+}
+
+   
--- commands/whois.js
+++ commands/whois.js
@@ -2,49 +2,54 @@ const Discord = require('discord.js')
 const getTimePassed = require('../utils/getTimeFromTimestamp')
 module.exports = {
     name: "whois",
-    miniDescription: 'Get information about a user, channel or server.',
-    description: 'Get information about a user, channel or server.',
-    usage: '<query>',
-    minArgs: 1,
+    miniDescription: 'Get information about a user.',
+    description: 'Get information about a user.',
+    usage: '[query]',
     commands: ['whois'],
-    callback:  (message, args, text, client) => {
+    callback: async (message, args, text, client) => {
         var query = args.join(' ')
-        if (query.startsWith('<@') && query.endsWith('>')) { // mention
-            query = query.replace(/[<@!>]/g, '')
-            
-            client.users.fetch(query).then(user => {
-               
-                var gUser = client.guilds.cache.get(message.guild.id).members.cache.get(user.id)
-                const embed = new Discord.MessageEmbed()
-                    .setColor('#0099ff')
-                    .setTitle(`whois for ${user.username}`)
-                    .setDescription(`${user.username}#${user.discriminator}`)
-                    .setThumbnail(user.avatarURL())
-                    .addField('Avatar URL', user.avatarURL())
-                    .addField('ID', user.id, false)
-                    .addField('Account Creation Date', `${getTimePassed(Date.now() - user.createdAt)} ago (${new Date(user.createdAt).toUTCString()})`)
-                    .addField(`Joined ${message.guild.name} at`, new Date(gUser.joinedAt).toUTCString())
-                    
-                return message.reply({embeds: [embed], allowedMentions: {repliedUser: false}})
-            }).catch(err => {
-                message.reply({content: 'Could not find user.', allowedMentions: {repliedUser: false}})
-            })
+
+        var query = args.join(' ')
+        if(!query)
+        {
+            getInfo(message.author, message)
         }
-        if (query.startsWith('<#') && query.endsWith('>')) { // channel
+        else if (query.startsWith('<@') && query.endsWith('>')) { // mention
             query = query.replace(/[<#!>]/g, '')
             
-           const channel = message.guild.channels.cache.get(query)
-           const embed = new Discord.MessageEmbed()
-           .setColor('#0099ff')
-           .setTitle(`channel information for ${channel.name}`)
-           .addField('Type', channel.type)
-           .addField('ID', channel.id, false)
-           .addField('Creation Date', `${getTimePassed(Date.now() - channel.createdAt)} ago (${new Date(channel.createdAt).toUTCString()})`)
-            return message.reply({embeds: [embed], allowedMentions: {repliedUser: false}})
+           const user = await message.guild.users.cache.get(query)
+           getInfo(user, message)
           
         }
-       
+        else
+        {
+            const user = await message.guild.users.cache.get(query)
+           getInfo(user, message)
+        }
         
 
     }
 }
+
+
+
+
+
+function getInfo(user, message)
+{
+    if(!user) {
+        return message.reply({content: 'Could not find user.', allowedMentions: {repliedUser: false}})
+    }
+    const embed = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle(`whois ${user.username}`)
+    .setDescription(`${user.username}#${user.discriminator}`)
+    .setThumbnail(user.avatarURL())
+    .addField('Avatar URL', user.avatarURL())
+    .addField('ID', user.id, false)
+    .addField('Account Creation Date', `${getTimePassed(Date.now() - user.createdAt)} ago (${new Date(user.createdAt).toUTCString()})`)
+    .addField(`Joined ${message.guild.name} at`, new Date(user.joinedAt).toUTCString())
+    
+return message.reply({embeds: [embed], allowedMentions: {repliedUser: false}})
+}
+   
