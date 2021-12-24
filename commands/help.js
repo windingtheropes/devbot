5e49a55f77506b584c993c8762181652ae313336
windingtheropes <windingtheropes@gmail.com>
2021-12-23 21:30:07 -0500

    start help rewrite

--- commands/help.js
+++ commands/help.js
@@ -6,85 +6,28 @@ module.exports = {
     description: "Get help for using devbot. To get specific command help, add one of the command's aliases as the first argument. For generic help, run the command without any arguments.",
     usage: "[command]",
     callback: async (message, args, text, client, prefix, allCommands, commandList) => {
-        let commandName
         if(args[0])
         {
-            commandName = args[0].toLowerCase()
-        }
-        if(commandName)
-        {
+            const query = args[0].toLowerCase()
+            args.shift()
+            const command = allCommands[query]
             
-            const command = allCommands[commandName]
             
-            if(command)
-            {   
-                if(command.enabled === false)
-                {
-                    
-                }
-                if(command.description)
-                {
-                    var exampleUsageString =''
-                    
-                    if(command.exampleUsage)
-                    {
-                        command.exampleUsage.forEach(use => {
-                            exampleUsageString = `${exampleUsageString}\n${prefix}${commandName} ${use}`
-                        })
-                    }
-                    else
-                    {
-                        exampleUsageString = "unavailable"
-                    }
-                   
-                    const helpEmbed = new Discord.MessageEmbed()
-                        .setColor('#0099ff')
-                        .setTitle('Command help')
-                        .setAuthor('devbot')
-                        .setDescription(command.description)
-                        .addField('Usage', `${prefix}${commandName} ${command.usage}`, false)
-                        .addField('Aliases', command.commands.join(', '), false)
-                        .addField('Example Usage', `${exampleUsageString}`, false)
-
-                    
-                   
-                    await message.channel.send({embeds: [helpEmbed]})
-                }
-                else
-                {
-                    message.channel.send('No help available for command `' + `${commandName}` + '`')
-                }
-            }
-            else
-            {
-                message.channel.send('Could not find a command with alias `' + `${commandName}` + '`')
-            }
+            const embed = new Discord.MessageButton()
+            
         }
         else
         {
-            const helpEmbed = new Discord.MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle('Help')
-            .setAuthor('devbot')
-            .setDescription('Devbot commands are fairly simple, commands and arguments are not sensitive to case. Some commands have multiple aliases.')
-            .addField('Usage', 'Arguments wrapped in <> are required, while arguments wrapped in [] are optional. Note: if you use one optional argument you must use them all in the correct order. The brackets are for example purposes only, do not use them when actually running a command.', false)
-        
-            // commandList.forEach(cmd => {
-            //     const aliases = cmd[0].toString()
-            //     var miniDescription = cmd[1]
-            //     if(miniDescription === '' || undefined || null)
-            //     {
-            //         miniDescription = 'unavailable'
-            //     }
-            //     const listed = cmd[2]
 
-            //     if(listed)
-            //     {
-            //         helpEmbed.addField(aliases, miniDescription, false)
-            //     }
-            // })
-            await message.channel.send({embeds: [helpEmbed]})
+            const embed = new Discord.MessageEmbed()
+            embed.setTitle('Devbot Help')
+            embed.setDescription(`Devbot is a multipurpose bot with various features. To get help for a specific command, run \`${prefix}help [command]\`\n\nCommand syntax: \`${prefix}<command> <required> [optional]\`\n\nCertain more advanced commands may include more syntax indicators.`)
+            message.reply({embeds: [embed], components: [row]})
         }
+
+      
+        
+        
     }
     
 }
