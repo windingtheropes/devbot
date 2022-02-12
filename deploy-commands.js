7ba8e1db94528c72d2a62466acefc36f0ca57f87
windingtheropes <windingtheropes@gmail.com>
2022-02-12 16:48:59 -0500

    add updated command handler

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,16 +3,36 @@ const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
 const dotenv = require('dotenv');
 const fs = require('fs')
+const path = require('path')
 
 dotenv.config()
 
 const commands = []
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
-}
+// for (const file of commandFiles) {
+// 	const command = require(`./commands/${file}`);
+// 	commands.push(command.data.toJSON());
+// }
+
+
+  
+    const readCommands = (dir) => {
+      const files = fs.readdirSync(path.join(__dirname, dir));
+  
+      for (const file of files) {
+        const stat = fs.lstatSync(path.join(__dirname, dir, file))
+  
+        if (stat.isDirectory()) {
+          readCommands(path.join(dir, file))
+        } else if (file !== 'command-handler.js' && file.endsWith('.js')) {
+          const command = require(path.join(__dirname, dir, file))
+          commands.push(command.data.toJSON());
+        }
+      }
+    }
+  
+    readCommands('commands')
 
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // deps
 const { Client, Collection, Intents } = require('discord.js');
 const dotenv = require('dotenv');
-const {version, prerelease} = require('./versioninfo.json')
+const { version, prerelease } = require('./versioninfo.json')
 const fs = require('fs');
 const mongo = require('./utils/mongo')
 const path = require('path');
 
 // utils
-const commandHandler = require('./command-handler.js')
 
 // configs
 dotenv.config()
 
 // vars
+const commandBase = require('./commands/command-handler.js')
 
 const messageListenerBaseFile = 'messageListener-base.js'
 const messageListenerBase = require(`./features/message/${messageListenerBaseFile}`)
@@ -28,132 +28,141 @@ const vcStateChangeListenerBase = require(`./features/vcStateChanged/${vcStateCh
 
 
 const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_VOICE_STATES,
-        Intents.FLAGS.GUILD_PRESENCES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
-    ]
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_PRESENCES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+  ]
 })
-
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-
-	client.commands.set(command.data.name, command);
-}
 
 client.on('ready', async () => {
-    console.log(`devbot version ${version}\nCreated by windingtheropes${prerelease ? '\nPre-release version; expect bugs and instabilities.\n' : '\n'}`)
-    await mongo().then(async mongoose => {
-        try {
-           console.log("Connected to mongo.\n")
-         } 
-         catch
-         {
-           return console.log("Error connecting to mongo.")
-         }
-         finally {
-           mongoose.connection.close()
-         }
-       })
-    
-    // start command listener
-    commandHandler.listen(client)
+  console.log(`devbot version ${version}\nCreated by windingtheropes${prerelease ? '\nPre-release version; expect bugs and instabilities.\n' : '\n'}`)
+  await mongo().then(async mongoose => {
+    try {
+      console.log("Connected to mongo.\n")
+    }
+    catch
+    {
+      return console.log("Error connecting to mongo.")
+    }
+    finally {
+      mongoose.connection.close()
+    }
+  })
 
-    // import other listeners
-    messageListenersImport()
-    userJoinListenersImport()
-    vcStateChangeListenersImport()
-    pinsListenerImport()
+  // start command listener
+  commandBase.listen(client)
+  commandsImport()
 
-    // start other listeners
-    messageListenerBase.listen(client)
-    userJoinListenerBase.listen(client)
-    vcStateChangeListenerBase.listen(client)
-    pinListenerBase.listen(client)
+  // import other listeners
+  messageListenersImport()
+  userJoinListenersImport()
+  vcStateChangeListenersImport()
+  pinsListenerImport()
+
+  // start other listeners
+  messageListenerBase.listen(client)
+  userJoinListenerBase.listen(client)
+  vcStateChangeListenerBase.listen(client)
+  pinListenerBase.listen(client)
 
 })
 
-function pinsListenerImport()
-{
+function commandsImport() {
+  client.commands = new Collection();
+
+  const readCommands = (dir) => {
+    const files = fs.readdirSync(path.join(__dirname, dir));
+
+    for (const file of files) {
+      const stat = fs.lstatSync(path.join(__dirname, dir, file))
+
+      if (stat.isDirectory()) {
+        readCommands(path.join(dir, file))
+      } else if (file !== 'command-handler.js' && file.endsWith('.js')) {
+        const command = require(path.join(__dirname, dir, file))
+        client.commands.set(command.data.name, command);
+      }
+    }
+  }
+
+  readCommands('commands')
+}
+
+function pinsListenerImport() {
   //dynamically import commands
 
   const readCommands = (dir) => {
     const files = fs.readdirSync(path.join(__dirname, dir))
 
     for (const file of files) {
-        const stat = fs.lstatSync(path.join(__dirname, dir, file))
+      const stat = fs.lstatSync(path.join(__dirname, dir, file))
 
-        if (stat.isDirectory()) {
-            readCommands(path.join(dir, file))
-        } else if (file !== pinListenerBaseFile) {
-            const option = require(path.join(__dirname, dir, file))
-            pinListenerBase(option)
-        }
+      if (stat.isDirectory()) {
+        readCommands(path.join(dir, file))
+      } else if (file !== pinListenerBaseFile) {
+        const option = require(path.join(__dirname, dir, file))
+        pinListenerBase(option)
+      }
     }
-}
+  }
 
   readCommands('features/pinsUpdate')
 }
 
-function messageListenersImport()
-{
+function messageListenersImport() {
   //dynamically import commands
   const readListeners = (dir) => {
     const files = fs.readdirSync(path.join(__dirname, dir))
     for (const file of files) {
-        const stat = fs.lstatSync(path.join(__dirname, dir, file))
+      const stat = fs.lstatSync(path.join(__dirname, dir, file))
 
-        if (stat.isDirectory()) {
-            readListeners(path.join(dir, file))
-        } else if (file !== messageListenerBaseFile) {
-            const callback = require(path.join(__dirname, dir, file))
-            messageListenerBase(callback)
-        }
+      if (stat.isDirectory()) {
+        readListeners(path.join(dir, file))
+      } else if (file !== messageListenerBaseFile) {
+        const callback = require(path.join(__dirname, dir, file))
+        messageListenerBase(callback)
+      }
     }
-}
+  }
 
   readListeners('features/message')
 }
 
-function userJoinListenersImport()
-{
+function userJoinListenersImport() {
   const readListeners = (dir) => {
     const files = fs.readdirSync(path.join(__dirname, dir))
     for (const file of files) {
-        const stat = fs.lstatSync(path.join(__dirname, dir, file))
+      const stat = fs.lstatSync(path.join(__dirname, dir, file))
 
-        if (stat.isDirectory()) {
-            readListeners(path.join(dir, file))
-        } else if (file !== userJoinListenerBaseFile) {
-            const callback = require(path.join(__dirname, dir, file))
-            userJoinListenerBase(callback)
-        }
+      if (stat.isDirectory()) {
+        readListeners(path.join(dir, file))
+      } else if (file !== userJoinListenerBaseFile) {
+        const callback = require(path.join(__dirname, dir, file))
+        userJoinListenerBase(callback)
+      }
     }
-}
+  }
   readListeners('features/userJoin')
 }
 
-function vcStateChangeListenersImport()
-{
+function vcStateChangeListenersImport() {
   const readListeners = (dir) => {
     const files = fs.readdirSync(path.join(__dirname, dir))
     for (const file of files) {
-        const stat = fs.lstatSync(path.join(__dirname, dir, file))
+      const stat = fs.lstatSync(path.join(__dirname, dir, file))
 
-        if (stat.isDirectory()) {
-            readListeners(path.join(dir, file))
-        } else if (file !== vcStateChangeListenerBaseFile) {
-            const callback = require(path.join(__dirname, dir, file))
-            vcStateChangeListenerBase(callback)
-        }
+      if (stat.isDirectory()) {
+        readListeners(path.join(dir, file))
+      } else if (file !== vcStateChangeListenerBaseFile) {
+        const callback = require(path.join(__dirname, dir, file))
+        vcStateChangeListenerBase(callback)
+      }
     }
-}
+  }
   readListeners('features/vcStateChanged')
 }
 
