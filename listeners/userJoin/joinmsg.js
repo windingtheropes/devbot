2fabf1c9d43dc40c9703df20a728fbfbe6b9bccc
Jack Anderson <jack@windingtheropes.tech>
2022-09-13 14:04:27 -0400

    add devbot under utils, add formatting string helper

--- listeners/userJoin/joinmsg.js
+++ listeners/userJoin/joinmsg.js
@@ -1,11 +1,11 @@
 const {greeter} = require("../../models/index")
+const {formatUserGuildString} = require("../../utils/devbot")
 module.exports = {
     type: 'join',
     callback: async (member) => {
         const serverData = await greeter.findOne({ where: { guildId: member.guild.id } })
         if (!serverData.joinMessageEnabled) return
 
-        const formatted = serverData.joinMessage.replace(/{user}/g, member.user.username).replace(/{mention}/g, `<@!${member.user.id}>`).replace(/{server}/g, member.guild.name)
-        member.guild.channels.cache.get(serverData.channelId).send(formatted)
+        member.guild.channels.cache.get(serverData.channelId).send(formatUserGuildString(serverData.joinMessage, member))
     }
 }
--- listeners/userJoin/leavemsg.js
+++ listeners/userJoin/leavemsg.js
@@ -1,11 +1,11 @@
 const {greeter} = require("../../models/index")
+const {formatUserGuildString} = require("../../utils/devbot")
 module.exports = {
     type: 'leave',
     callback: async (member) => {
         const serverData = await greeter.findOne({ where: { guildId: member.guild.id } })
         if (!serverData.leaveMessageEnabled) return
 
-        const formatted = serverData.leaveMessage.replace(/{user}/g, member.user.username).replace(/{mention}/g, `<@!${member.user.id}>`).replace(/{server}/g, member.guild.name)
-        member.guild.channels.cache.get(serverData.channelId).send(formatted)
+        member.guild.channels.cache.get(serverData.channelId).send(formatUserGuildString(serverData.leaveMessage, member))
     }
 }
