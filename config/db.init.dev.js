96763cb7714363c4e8849f7ef2e1bfa8033f36d3
Jack Anderson <jack@windingtheropes.tech>
2022-11-18 15:14:35 -0500

    revert es6

---
+++ config/db.init.dev.js
@@ -0,0 +1,7 @@
+const db = require('../models/index.js');
+
+module.exports = () => {
+    db.sequelize.sync().then(() => { // {force:true} wioll clear db every start
+        console.log('Database synced.');
+    })
+}
--- config/db.init.dev.ts
+++
@@ -1,7 +0,0 @@
-import db from '../models/index.js';
-
-module.exports = () => {
-    db.sequelize.sync().then(() => { // {force:true} wioll clear db every start
-        console.log('Database synced.');
-    })
-}
